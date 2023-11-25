b887b845ae5db097f9f5afc6f7a1465b54983d20
random0712 <patricknuvem9@gmail.com>
2023-11-25 20:09:06 -0300

    ci(controller): disable cors

--- src/main/java/com/anhembi/a3/controller/ChannelController.java
+++ src/main/java/com/anhembi/a3/controller/ChannelController.java
@@ -3,13 +3,15 @@ package com.anhembi.a3.controller;
 import com.anhembi.a3.model.Channel;
 import com.anhembi.a3.service.ChannelService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController()
+@CrossOrigin("*")
+@RestController
 @RequestMapping("/api/v1/channel")
 @RequiredArgsConstructor
 public class ChannelController {
--- src/main/java/com/anhembi/a3/controller/SalesController.java
+++ src/main/java/com/anhembi/a3/controller/SalesController.java
@@ -3,12 +3,14 @@ package com.anhembi.a3.controller;
 import com.anhembi.a3.model.Sales;
 import com.anhembi.a3.service.SalesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/sales")
 @RequiredArgsConstructor
